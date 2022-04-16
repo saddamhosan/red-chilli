5aeaf65e15d859da521d57a6352ca004e8578728
saddamhosan <contact.saddamhosan.com>
2022-04-16 17:40:58 +0600

    update

--- src/Component/RequireAuth.js
+++ src/Component/RequireAuth.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { ClipLoader } from "react-spinners";
 import auth from '../firebase.init';
 
 const RequireAuth = ({children}) => {
-    const [user] = useAuthState(auth);
+    const [user,loading] = useAuthState(auth);
     const location=useLocation()
+    if(loading){
+        return <p className="text-center mt-24 "><ClipLoader /></p>
+         
+    }
     if(!user){
         return <Navigate to='/signIn'state={{from:location}}replace />
     }
